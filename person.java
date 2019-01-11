dc95c265d9ffb56bdd5712a3cffeb1b900a7fed3
olejoh1 <43674394+olejoh1@users.noreply.github.com>
2019-01-11 12:34:11 +0100

    .

--- person.java
+++ person.java
@@ -1,20 +1,35 @@
 import java.util.Scanner;
 
-public class person{
-
+public class Person {
+//Må også oppdattere filnavnet til det samme navnet som på klassen
     public static void main(String[] args) {
-
+        // Oppretter og instansierer et objekt av typen Scanner
         Scanner scanner = new Scanner(System.in);
 
+        // Printer ut til konsollen
         System.out.println("Skriv inn fornavn: ");
+        // Leser input fra konsollen
         String fornavn = scanner.nextLine();
 
         System.out.println("Skriv inn etternavn: ");
+
         String etternavn = scanner.nextLine();
 
         System.out.println("Skriv inn aldedr: ");
         int alder = scanner.nextInt();
 
-        System.out.println("Ditt navn er: " + fornavn + " " + etternavn +"\n" + "Din alder er: " + alder);
     }
+
+    Person(String fornavn, String etternavn, int alder){
+
+        Person person1 = new Person(fornavn, etternavn, alder);
+
+    }
+
+    public void printPerson() {
+
+        System.out.println(person1);
+
+    }
+
 }
