86d3681e16a9cfca4edb0aed166b915bf0ce480e
olejoh1 <43674394+olejoh1@users.noreply.github.com>
2019-01-11 15:18:54 +0100

    .

--- oppg3.java
+++ oppg3.java
@@ -1,15 +1,11 @@
 import java.util.Scanner;
 
-public class oppg3 extends Thread{
+public class oppg3 {
 
     public static String fornavn, etternavn;
     public static int alder;
 
     public static void main(String[] args) {
-
-        Thread t = Thread.currentThread();
-
-        t.setPriority(10);
 
         Scanner scanner = new Scanner(System.in);
 
@@ -24,18 +20,7 @@ public class oppg3 extends Thread{
         System.out.println("Skriv inn aldedr: ");
         int alder = scanner.nextInt();
 
-        ChildThread ct = new ChildThread();
-
-        ct.setPriority(1);
-
-        ct.start();
-
-    }
-
-    class ChildThread extends Thread{
-        public void run(){
-        System.out.println("Navn: " + oppg3.fornavn + " " + oppg3.etternavn + "\n" + "Alder: " + oppg3.alder);
-        }
+        System.out.println("Navn: " + fornavn + " " + etternavn + "\n" + "Alder: " + alder);
 
     }
 
